c395c84cc719803782eb9c85d208d22bf915de65
Lorisssss <113813846+Lorisssss@users.noreply.github.com>
2023-05-31 21:19:46 -0400

    Update Comparehp.lua

--- games/SPTS Endless/Comparehp.lua
+++ games/SPTS Endless/Comparehp.lua
@@ -1,6 +1,6 @@
 local players = game:getservice("players")
 local myplayer = players.localplayer
-local myMaxHP = myplayer.character:WaitForChild("humanoid").maxhp.value
+local maxhp = myplayer.character:WaitForChild("humanoid").maxhp.value
 
 for _, player in ipairs(players:getplayers()) do
 	local character = player.character
